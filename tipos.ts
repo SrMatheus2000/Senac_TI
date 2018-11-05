9664087932516f1b15c9bb87cfbe37617ea0d8c7
SrMatheus2000 <matheuswandscheer2000@hotmail.com>
2018-11-05 21:32:15 -0200

    tipos

--- tipos.ts
+++ tipos.ts
@@ -1,4 +1,11 @@
-let OlaMundo = (teste: string) => {
-    console.log(teste);
-};
-OlaMundo("Olá Mundo");
+// let OlaMundo = (teste: string) => {
+//     console.log(teste);
+// };
+// OlaMundo("Olá Mundo");
+
+
+let mensagem: string = "ola mundo"
+let numero: number = 2
+let boleano: boolean = false
+let listadepalavras: string Array<string> = ['1', '2']
+let listadenumeros: string Array<number> = [1, 2]
