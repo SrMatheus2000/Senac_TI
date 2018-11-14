685d324739681449cd9064e495dc509167002c80
SrMatheus2000 <matheuswandscheer2000@hotmail.com>
2018-11-14 21:53:19 -0200

    zoo

--- Tipos/Zoologico/app.js
+++ Tipos/Zoologico/app.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const animal_1 = __importDefault(require("./animal"));
 const zoologico_1 = __importDefault(require("./zoologico"));
+let animais = [];
+// let animais = []
+// let animais = new Array
 let toto = new animal_1.default("toto", 50, 2);
 let aranha = new animal_1.default("aranha", 0, 4);
 let peixe = new animal_1.default("peixe", 20, 2);
 let zebra = new animal_1.default("zebra", 30, 2);
 let gato = new animal_1.default("gato", 40, 2);
-let animais = new Array;
 animais.push(toto);
 animais.push(aranha);
 animais.push(peixe);
--- Tipos/Zoologico/app.ts
+++ Tipos/Zoologico/app.ts
@@ -1,13 +1,15 @@
 import animal from "./animal"
 import zoologico from "./zoologico";
 
+let animais: animal[] = []
+// let animais = []
+// let animais = new Array
+
 let toto = new animal("toto", 50, 2)
 let aranha = new animal("aranha", 0, 4)
 let peixe = new animal("peixe", 20, 2)
 let zebra = new animal("zebra", 30, 2)
 let gato = new animal("gato", 40, 2)
-
-let animais = new Array
 
 animais.push(toto)
 animais.push(aranha)
