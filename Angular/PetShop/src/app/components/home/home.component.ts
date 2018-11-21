ed1933154c3879ad36d99507475768dda68dfbf0
SrMatheus2000 <matheuswandscheer2000@hotmail.com>
2018-11-21 21:22:15 -0200

    String Interpolation

--- Angular/PetShop/src/app/components/home/home.component.ts
+++ Angular/PetShop/src/app/components/home/home.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ["./home.component.css"]
 })
 export class MainComponent {
-  title = "main";
+  // data binding
+  
+  // string interpolation
+  // title = "home!!!";
+
+  // property binding
+  // caminhoIMG = "assets/img/";
+  // cerebro = this.caminhoIMG + "cerebro.jpg";
+
+  // event binding
+  // imagem = this.caminhoIMG + "cerebro.jpg"
+  // exibir(){
+  //   this.imagem = this.caminhoIMG + "cerebro.jpg";
+  // }
+  // ocultar(){
+  //   this.imagem = ''
+  // }
+
+  // f_imagem(){
+  //   if (this.imagem == this.caminhoIMG + "cerebro.jpg")
+  //     this.imagem = ""
+  //   else
+  //     this.imagem = this.caminhoIMG + "cerebro.jpg"
+  // }
+  texto1 = "Olá Mundo"
+  texto2 = this.texto1
+  disparar(valor: Event){
+    // console.log(this.a);
+    // console.log(valor);
+    if (valor["data"] === null) {
+      this.texto2 = this.texto2.substr(0, this.texto2.length-1);
+    } else {
+      this.texto2 += valor["data"];
+    }
+    // this.texto2 = valor
+  }
+
+
+
 }
