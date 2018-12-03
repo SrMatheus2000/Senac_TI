58a179d2e86b260b660d0ac14aa63e1364546575
SrMatheus2000 <matheuswandscheer2000@hotmail.com>
2018-12-03 21:56:53 -0200

    petshop

--- Angular/PetShopTrabalho-App/src/app/app-routing.module.ts
+++ Angular/PetShopTrabalho-App/src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContatoComponent } from './components/contato/contato.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
 
 const routes: Routes = [];
 
@@ -8,3 +13,12 @@ const routes: Routes = [];
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const RotaConstante: Routes = [
+  { path: "contato", component: ContatoComponent },
+  { path: "galeria", component: GaleriaComponent },
+  { path: "inicio", component: InicioComponent },
+  { path: "produtos", component: ProdutosComponent },
+  { path: "servicos", component: ServicosComponent },
+  { path: "**", redirectTo: "first" }
+];
--- Angular/PetShopTrabalho-App/src/app/app.module.ts
+++ Angular/PetShopTrabalho-App/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RotaConstante } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { ServicosComponent } from './components/servicos/servicos.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContatoComponent,
+    GaleriaComponent,
+    InicioComponent,
+    MenuComponent,
+    ProdutosComponent,
+    ServicosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(RotaConstante)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
